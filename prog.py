038ffb175921f0654993b0fcc6d236c4098cdff0
OridinOver <maxpetstv@gmail.com>
2025-05-30 18:02:48 +0300

    Add files via upload

--- prog.py
+++ prog.py
@@ -1,6 +1,40 @@
-import random
-symvol = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-size = int(input('введите размер пароля Цифрами'))
-for i in range (size):
-    password = random.randint(symvol)
-print (password) 
+import telebot
+from bot_logic import gen_pass, gen_emodji, flip_coin , throve_dice # Импортируем функции из bot_logic
+
+# Замени 'TOKEN' на токен твоего бота
+bot = telebot.TeleBot ('7595394213:AAFLmnD0hu17WQw2Zy3yWmglhbx9UP1EahE')
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши команду /hello, /bye, /pass, /emodji или /coin или /dice  ")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['pass'])
+def send_password(message):
+    password = gen_pass(10)  # Устанавливаем длину пароля, например, 10 символов
+    bot.reply_to(message, f"Вот твой сгенерированный пароль: {password}")
+
+@bot.message_handler(commands=['emodji'])
+def send_emodji(message):
+    emodji = gen_emodji()
+    bot.reply_to(message, f"Вот эмоджи': {emodji}")
+
+@bot.message_handler(commands=['coin'])
+def send_coin(message):
+    coin = flip_coin()
+    bot.reply_to(message, f"Монетка выпала так: {coin}")
+
+@bot.message_handler(commands=['dice'])
+def send_dice(message):
+    dice = throve_dice()
+    bot.reply_to(message, f"на кубике -  {dice}")
+
+# Запускаем бота
+bot.polling()
